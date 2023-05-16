4fe6f1fac9aee35fccc48d0787589ae4f0e08795
Jun7414 <eric08000800@gmail.com>
2023-05-17 02:20:58 +0800

    Gauss eli for GF(2)

--- linear_algebra.py
+++ linear_algebra.py
@@ -40,3 +40,33 @@ def Gauss_elimination(A, b=None, return_b=False, pivot=False):
     if return_b:
         return A, b
     return A
+
+
+def GF2_Gauss_elimination(A):
+    n, m = A.shape
+    
+    def swap_row(i,j):
+        temp = np.copy(A[i])
+        A[i] = A[j]
+        A[j] = temp
+
+    i = 0
+    j = 0
+    while i < n and j < m:
+        # Find the ith pivot
+        i_max = np.argmax(A[i:, j]%2) + i
+        if A[i_max, j]%2 == 0 :
+            j += 1
+        else:
+            swap_row(i, i_max)
+            # Gauss elimination
+            for k in range(n):
+                if A[k,j]%2 == 0 or k == i:
+                        pass
+                else:
+                    for l in range(j, m):
+                        A[k,l] = (A[k,l] + A[i,l])%2
+            i += 1
+            j += 1    
+
+    return A
