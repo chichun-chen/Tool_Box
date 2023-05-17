fc4342f4a2f2575cf262cf8ed00da7b8834a2280
Jun7414 <eric08000800@gmail.com>
2023-05-17 13:44:33 +0800

    Gram_Schmidt

--- basis.py
+++ basis.py
@@ -2,6 +2,7 @@ import numpy as np
 
 def Gram_Schmidt(X):
     n, dim = X.shape
+    assert n <= dim , "Vectors should not be more than the dimension of space."    
 
     for i in range(n):
         for j in range(i):
